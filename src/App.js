46bfbccf7859bf198283c83df9d71c7f574aa21b
Amoorer75 <Amoorer127@gmail.com>
2022-02-08 21:30:49 -0500

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('black')
+
+  console.log(color)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ backgroundColor:color, height:"150px", width:'150'}}>
+
+      </div>
+      <input type='text' placeholder="color" onChange={text => setColor(text.target.value)}>
+       
+      </input>
+
+  
     </div>
   );
 }
